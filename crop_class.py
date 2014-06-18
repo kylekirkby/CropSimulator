04e88b5cbbb0916e27036feb849565b339c418af
kylekirkby <steve@zencoder.com>
2014-06-18 22:47:54 +0100

    Task 6 - Added Main menu

--- crop_class.py
+++ crop_class.py
@@ -60,17 +60,88 @@ def auto_grow(crop, days):
         water = random.randint(1,10)
         crop.grow(light,water)
 
+def manual_grow(crop):
+   valid = False
+   while not valid:
+      try:
+         light = int(input("Please enter a light value (1-10): "))
+         if 1 <= light <= 10:
+            valid = True
+         else:
+            print("Value entered not valid - please enter a light value between 1 and 10")
+      except ValueError:
+         print("Value entered not valid - please enter a light value between 1 and 10")
+   valid = False
+   while not valid:
+      try:
+         water = int(input("Please enter a water value (1-10): "))
+         if 1 <= light <= 10:
+            valid = True
+         else:
+            print("Value entered not valid - please enter a water value between 1 and 10")
+      except ValueError:
+         print("Value entered not valid - please enter a water value between 1 and 10")
 
+   crop.grow(light, water)
+         
 #tests the functionality of the class manually
-        
+
+
+
+def display_menu():
+   print("1. Grow manually over 1 day")
+   print("2. Grow automatically over 30 days")
+   print("3. Report Status")
+   print("0. Exit Program")
+   print()
+   print("Please select an option from the above menu")
+
+def get_menu_choice():
+   option_valid = False
+   while not option_valid:
+      try:
+         choice = int(input("Option Selected: "))
+         if 0 <= choice <= 4:
+            option_valid = True
+         else:
+            print("Please enter a valid option")
+      except ValueError:
+         print("Please enter a valid option")
+   return choice
+
+def manage_crop(crop):
+   
+   print("This is the crop management program")
+   print()
+   noexit = True
+   while noexit:
+      display_menu()
+      option = get_menu_choice()
+      print()
+      if option == 1:
+         manual_grow(crop)
+         print()
+      elif option == 2:
+         auto_grow(crop, 30)
+         print()
+      elif option == 3:
+         print(crop.report())
+         print()
+      elif option == 0:
+         noexit = False
+         print()
+   print("Thank you for using the crop management program!")
+
+   
 def main():
+   
+   
     #instantiate class
 
-    new_crop = Crop(1,4,3)
-    print(new_crop.report())
-    print(new_crop.needs())
-    auto_grow(new_crop, 30)
-    print(new_crop.report())
+   new_crop = Crop(1,4,3)
+
+   manage_crop(new_crop)
+
     
 
 if __name__ == "__main__":
